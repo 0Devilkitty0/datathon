261dec4dcc7e6eff1fafeea7a22f784fab950879
0Devilkitty0 <147805554+0Devilkitty0@users.noreply.github.com>
2025-06-01 15:48:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import warnings
 warnings.filterwarnings('ignore')
 
 base = pd.read_csv('D:/Users/tonyn/Desktop/da_sci_4th/datathon/DATA/Base.csv')
-base_df_copy = base.copy()
+base_copy = base.copy()
 
 var1 = pd.read_csv('D:/Users/tonyn/Desktop/da_sci_4th/datathon/DATA/Variant I.csv')
 var1_copy = var1.copy()
@@ -37,13 +37,14 @@ def EDA_dataset(df):
 
     return df
 # %%
-base_df_copy = EDA_dataset(base_df_copy)
+base_df_copy = EDA_dataset(base_copy)
 var1_copy = EDA_dataset(var1_copy)
 var2_copy = EDA_dataset(var2_copy)
 var3_copy = EDA_dataset(var3_copy)
 var4_copy = EDA_dataset(var4_copy)
 var5_copy = EDA_dataset(var5_copy)
-
+# %%
+base_df_copy
 # %%
 def one_hot(df):
     object_cols = ['source', 'device_os']
@@ -51,9 +52,53 @@ def one_hot(df):
 
     return df
 # %%
-base_df_copy = one_hot(base_df_copy)
+base_df_copy = one_hot(base_copy)
 var1_copy = one_hot(var1_copy)  
 var2_copy = one_hot(var2_copy)
 var3_copy = one_hot(var3_copy)
 var4_copy = one_hot(var4_copy)
 var5_copy = one_hot(var5_copy)
+
+#%%
+def test_split(df):
+    df.loc[(df['customer_age'] < 50) & (df['fraud_bool'] == 0), 'group'] = 0
+    df.loc[(df['customer_age'] < 50) & (df['fraud_bool'] == 1), 'group'] = 1
+    df.loc[(df['customer_age'] >= 50) & (df['fraud_bool'] == 0), 'group'] = 2
+    df.loc[(df['customer_age'] >= 50) & (df['fraud_bool'] == 1), 'group'] = 3
+
+    return df
+# %%
+base_copy = test_split(base_copy)
+var1_copy = test_split(var1_copy)
+var2_copy = test_split(var2_copy)
+var3_copy = test_split(var3_copy)
+var4_copy = test_split(var4_copy)
+var5_copy = test_split(var5_copy)
+
+#%%
+###100만개 데이터 중 10만개씩 sampking###
+from sklearn.model_selection import train_test_split
+def split_data(df):
+    df.loc[(df['customer_age'] < 50) & (df['fraud_bool'] == 0), 'group'] = 0
+    df.loc[(df['customer_age'] < 50) & (df['fraud_bool'] == 1), 'group'] = 1
+    df.loc[(df['customer_age'] >= 50) & (df['fraud_bool'] == 0), 'group'] = 2
+    df.loc[(df['customer_age'] >= 50) & (df['fraud_bool'] == 1), 'group'] = 3
+    df['group'] = df['group'].astype(int)
+
+    X = df.drop(columns=['group'])
+    y = df['group']
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.9, random_state=42, stratify=y)
+    X_train.reset_index(drop=True, inplace=True)
+    X_test.reset_index(drop=True, inplace=True)
+    y_train.reset_index(drop=True, inplace=True)
+    y_test.reset_index(drop=True, inplace=True)
+    
+    return X_train, X_test, y_train, y_test
+#%%
+base_sam, X_test, y_train, y_test = split_data(base_copy)
+var1_sam, X_test, y_train, y_test = split_data(var1_copy)
+var2_sam, X_test, y_train, y_test = split_data(var2_copy)
+var3_sam, X_test, y_train, y_test = split_data(var3_copy)
+var4_sam, X_test, y_train, y_test = split_data(var4_copy)
+var5_sam, X_test, y_train, y_test = split_data(var5_copy)
